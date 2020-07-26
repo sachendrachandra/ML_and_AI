2f35bdae3a691cad9ba263d03f4ed2e8eef02a9f
sachendrachandra <58558221+sachendrachandra@users.noreply.github.com>
2020-07-26 12:49:37 +0530

    Add files via upload

---
+++ Image_Captioning_WebApp/Caption_it.py
@@ -0,0 +1,171 @@
+#Generate Captions for a Fresh Image
+
+from pickle import load
+from numpy import argmax
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+from keras.models import load_model
+
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# load a pre-defined list of photo identifiers
+def load_set(filename):
+	doc = load_doc(filename)
+	dataset = list()
+	# process line by line
+	for line in doc.split('\n'):
+		# skip empty lines
+		if len(line) < 1:
+			continue
+		# get the image identifier
+		identifier = line.split('.')[0]
+		dataset.append(identifier)
+	return set(dataset)
+
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+	# load document
+	doc = load_doc(filename)
+	descriptions = dict()
+	for line in doc.split('\n'):
+		# split line by white space
+		tokens = line.split()
+		# split id from description
+		image_id, image_desc = tokens[0], tokens[1:]
+		# skip images not in the set
+		if image_id in dataset:
+			# create list
+			if image_id not in descriptions:
+				descriptions[image_id] = list()
+			# wrap description in tokens
+			desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+			# store
+			descriptions[image_id].append(desc)
+	return descriptions
+
+
+# load photo features
+def load_photo_features(filename, dataset):
+	# load all features
+	all_features = load(open(filename, 'rb'))
+	# filter features
+	features = {k: all_features[k] for k in dataset}
+	return features
+
+# covert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+	all_desc = list()
+	for key in descriptions.keys():
+		[all_desc.append(d) for d in descriptions[key]]
+	return all_desc
+
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+	lines = to_lines(descriptions)
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines)
+	return tokenizer
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+	lines = to_lines(descriptions)
+	return max(len(d.split()) for d in lines)
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+	# seed the generation process
+	in_text = 'startseq'
+	# iterate over the whole length of the sequence
+	for i in range(max_length):
+		# integer encode input sequence
+		sequence = tokenizer.texts_to_sequences([in_text])[0]
+		# pad input
+		sequence = pad_sequences([sequence], maxlen=max_length)
+		# predict next word
+		yhat = model.predict([photo,sequence], verbose=0)
+		# convert probability to integer
+		yhat = argmax(yhat)
+		# map integer to word
+		word = word_for_id(yhat, tokenizer)
+		# stop if we cannot map the word
+		if word is None:
+			break
+		# append as input for generating the next word
+		in_text += ' ' + word
+		# stop if we predict the end of the sequence
+		if word == 'endseq':
+			break
+	return in_text
+
+def extract_features(filename):
+	# load the model
+	model = VGG16()
+	# re-structure the model
+	model.layers.pop()
+	model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+	# model._make_predict_function()
+	# load the photo
+	image = load_img(filename, target_size=(224, 224))
+	# convert the image pixels to a numpy array
+	image = img_to_array(image)
+	# reshape data for the model
+	# print(image.shape[0])
+	# print(image.shape[1])
+	# print(image.shape[2])
+
+	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+	# image = image.reshape()
+
+	# prepare the image for the VGG model
+	image = preprocess_input(image)
+	# get features
+	feature = model.predict(image, verbose=0)
+	return feature
+
+def caption(img):
+	# load the tokenizer
+	tokenizer = load(open('tokenizer.pkl', 'rb'))
+	# pre-define the max sequence length (from training)
+	max_length = 34
+	# load the model
+	model = load_model('model_18.h5')
+	# model._make_predict_function()
+	# load and prepare the photograph
+	photo = extract_features(img)
+	# generate description
+	description = generate_desc(model, tokenizer, photo, max_length)
+	return description
+
+
+# # load the tokenizer
+# tokenizer = load(open('tokenizer.pkl', 'rb'))
+# # pre-define the max sequence length (from training)
+# max_length = 34
+# # load the model
+# model = load_model('model_18.h5')
+# # load and prepare the photograph
+# photo = extract_features('Sample_Image.jpg')
+# # generate description
+# description = generate_desc(model, tokenizer, photo, max_length)
+# print(description)
